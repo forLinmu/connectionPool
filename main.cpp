2cb16d1a63b5c8f91f1c935f996c9a3fcfa02820
forLinmu <1315961412@qq.com>
2025-12-19 17:25:56 +0800

    第一次修改

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void sort(int arr[],int size){
+
+
+}
+int main(){
+
+int arr[]={1,3,72,9,34};
+int size=sizeof(arr)/sizeof(arr[0]);
+sort(arr,size);
+for(int i:arr){
+	cout<<i<<" ";
+}
+cout<<endl;
+
+return 0;
+}
